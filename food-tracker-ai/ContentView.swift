8e9459e74b1db2285b35bbed6348866c2b9f126f
Ethan Shafran Moltz <33405530+emoltz@users.noreply.github.com>
2023-12-10 12:29:45 -0500

    nav view!

--- food-tracker-ai/ContentView.swift
+++ food-tracker-ai/ContentView.swift
@@ -6,21 +6,31 @@ struct ContentView: View {
     @Query private var meals: [Meal]
     
     var body: some View {
-        
-        List(meals) { meal in
-            VStack(alignment: .leading){
-                Text("Meal Name: \(meal.name)")
-                Text("Meal Calories: \(meal.totalCalorites)")
-                
+        NavigationStack{
+            
+            List(meals) { meal in
+                Section{
+                    VStack(alignment: .leading){
+                        Text("Meal Name: \(meal.name)")
+                        Text("Meal Calories: \(meal.totalCalorites)")
+                        
+                    }
+                } header: {
+                    Text("Meals")
+                }
+                .navigationTitle("Your Stuff")
             }
+            
+            
         }
+        
     }
 }
 
 #Preview {
     let config = ModelConfiguration(isStoredInMemoryOnly: true)
     let container = try! ModelContainer(for: Meal.self, Food.self, configurations: config)
-
+    
     // Insert dummy data
     let food1 = Food(name: "food1", date: Date.now, quantity: 10, calories: 10, protein: 10, sugars: 22.2)
     let food2 = Food(name: "food2", date: Date.now, quantity: 30, calories: 284.2, protein: 18.2, sugars: 11.1)
@@ -29,7 +39,7 @@ struct ContentView: View {
     container.mainContext.insert(food1)
     container.mainContext.insert(food2)
     container.mainContext.insert(meal)
-
+    
     return ContentView()
         .modelContainer(container)
 }
