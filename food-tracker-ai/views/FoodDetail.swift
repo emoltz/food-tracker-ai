1c1dea28c0a0bc23ce57fb9ce81746f76c4c8949
Ethan Shafran Moltz <33405530+emoltz@users.noreply.github.com>
2023-12-10 20:05:08 -0500

    more components

---
+++ food-tracker-ai/views/FoodDetail.swift
@@ -0,0 +1,18 @@
+//
+//  FoodDetail.swift
+//  food-tracker-ai
+//
+//  Created by Ethan Shafran Moltz on 12/10/23.
+//
+
+import SwiftUI
+
+struct FoodDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FoodDetail()
+}
---
+++ food-tracker-ai/views/MealDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  food-tracker-ai
+//
+//  Created by Ethan Shafran Moltz on 12/10/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
